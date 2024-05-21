feat, fix: adding masking, correcting intrinsics from file

LTRL/datasets/endonasal_dataset.py
@@ -29,7 +29,9 @@ class EndonasalDataset(data.Dataset):
                  num_scales,
                  is_train=False, 
                  img_ext='.png', 
-                 mask_path=''):
+                 mask_path='', 
+                 camera_intrinsics_pth='', 
+                 height_intrinsics=False):
         super(EndonasalDataset, self).__init__()
         
         self.filenames = []
@@ -41,16 +43,34 @@ class EndonasalDataset(data.Dataset):
             self.filenames.extend(frame_path)
         
         self.mask_path = mask_path
+        
         if len(self.mask_path)>0:
-            # load png mask image
+            """ # load png mask image
             mask_img = cv2.imread(self.mask_path)
             mask_img = cv2.cvtColor(mask_img, cv2.COLOR_BGR2GRAY)
             # convert img to mask
-            self.mask = cv2.threshold(mask_img, 127, 255, cv2.THRESH_BINARY)[1]
+            self.mask = cv2.threshold(mask_img, 127, 255, cv2.THRESH_BINARY)[1] """
+            mask_img = cv2.imread(self.mask_path)
+            # convert to grayscale and generate mask
+            gray = cv2.cvtColor(mask_img, cv2.COLOR_RGB2GRAY)
+            mask = np.zeros(gray.shape)
+            gray = np.array(gray, np.uint8)
+            # detect largest circle
+            circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 3, 100)
+            if circles is not None:
+                circles = np.round(circles[0, :]).astype("int")
+                (x, y, r) = circles[0:1][0]
+                print('circle found:',x, y, r-80)
+                cv2.circle(mask, (x, y), r-30, 255, -1)
+            self.mask = mask
+
+        # resize mask
+        self.resized_mask = cv2.resize(self.mask, (width, height), interpolation=cv2.INTER_NEAREST)
+        
         self.height = height
         self.width = width
         self.num_scales = num_scales
-        self.interp = Image.ANTIALIAS
+        self.interp = Image.LANCZOS
         self.frame_idxs = frame_idxs
         self.is_train = is_train
         self.to_tensor = transforms.ToTensor()
@@ -73,10 +93,35 @@ class EndonasalDataset(data.Dataset):
             self.resize[i] = transforms.Resize((self.height // s, self.width // s),
                                                interpolation=self.interp)
         self.load_depth = self.check_depth()
-        self.K = np.array([[0.76, 0, 0.47, 0],
+        
+        # camera intrinsics- either load from path or pre-set intrinsics
+        if len(camera_intrinsics_pth) > 0:
+            intrinsics = np.loadtxt(camera_intrinsics_pth, dtype=np.float32)
+            # normalising intrinsics
+            #self.K[0,:] /= 2*self.K[0,2]
+            #self.K[1,:] /= 2*self.K[1,2]
+            # multiply by new h and w
+            
+            if height_intrinsics:
+                intrinsics[0,:] /= 1920
+                intrinsics[1,:] /= 1080
+            else:
+                intrinsics[0,:] *= width/2
+                intrinsics[1,:] *= height/2
+            
+            self.K = np.eye(4, dtype=np.float32)
+            self.K [0:3,0:3] = intrinsics
+
+            print("--------> INTRINSICS", self.K)
+
+
+            
+
+        else:
+            self.K = np.array([[0.76, 0, 0.47, 0],
                            [0, 1.35, 0.45, 0],
                            [0, 0, 1, 0],
-                           [0, 0, 0, 1]], dtype=np.float32)    
+                           [0, 0, 0, 1]], dtype=np.float32)
 
     def __len__(self):
         return len(self.filenames)
@@ -100,25 +145,26 @@ class EndonasalDataset(data.Dataset):
             filename = "{:0>{width}}".format(frame_index + i, width=name_length)
             try:
                 img = Image.open(os.path.join(folderdir, filename+img_ext)).convert('RGB')
+                if len(self.mask_path)>0:
+                
+                    # resize mask to size of image
+                    #mask = cv2.resize(mask, (self.opt.height, self.opt.width))
+                    # make size of mask repeated to match batch and RGB (size 12, 3, x,y)
+
+                    #mask = np.tile(mask, (self.opt.batch_size, 3, 1, 1)).shape
+                    # convert img to np array
+                    img = np.array(img)
+                    # all areas outside mask should be set to 0 in predicted image
+                    img[self.mask==0]=0
+                    # convert back to PIL image
+                    img = Image.fromarray(img)
             except:
                 print(f'dataloader file name: {os.path.join(folderdir, filename+img_ext)}')
             if do_flip:
                 img = img.transpose(pil.FLIP_LEFT_RIGHT)
-
-            if len(self.mask_path)>0:
-                
-                # resize mask to size of image
-                #mask = cv2.resize(mask, (self.opt.height, self.opt.width))
-                # make size of mask repeated to match batch and RGB (size 12, 3, x,y)
-
-                #mask = np.tile(mask, (self.opt.batch_size, 3, 1, 1)).shape
-                # convert img to np array
-                img = np.array(img)
-                # all areas outside mask should be set to 0 in predicted image
-                img[self.mask==0]=0
-                # convert back to PIL image
-                img = Image.fromarray(img)
-
+            
+            
+            
             inputs[("color", i, -1)] = img
             
         if do_color_aug:

LTRL/trainer_end_to_end.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, division, print_function
 
 import time
 import json
+
+from tqdm import tqdm
 import datasets
 from datasets.endonasal_dataset import EndonasalDataset
 import networks
@@ -134,9 +136,9 @@ class Trainer:
             seqs_train = ['rec_1_endo', 'rec_2_endo', 'rec_10_endo', 'rec_8_endo', 'rec_9_endo']
             seqs_val = ['rec_3_endo', 'rec_11_endo']
             train_dataset = EndonasalDataset(
-                self.opt.data_path, seqs_train, self.opt.height, self.opt.width, self.opt.frame_ids, 4, is_train=True, img_ext=self.opt.img_ext, mask_path=self.opt.mask_path)
+                self.opt.data_path, seqs_train, self.opt.height, self.opt.width, self.opt.frame_ids, 4, is_train=True, img_ext=self.opt.img_ext, mask_path=self.opt.mask_path, camera_intrinsics_pth=self.opt.camera_intrinsics_pth, height_intrinsics=self.opt.height_intrinsics)
             val_dataset = EndonasalDataset(
-                self.opt.data_path, seqs_val, self.opt.height, self.opt.width, self.opt.frame_ids, 4, is_train=False, img_ext=self.opt.img_ext, mask_path=self.opt.mask_path)  
+                self.opt.data_path, seqs_val, self.opt.height, self.opt.width, self.opt.frame_ids, 4, is_train=False, img_ext=self.opt.img_ext, mask_path=self.opt.mask_path, camera_intrinsics_pth=self.opt.camera_intrinsics_pth, height_intrinsics=self.opt.height_intrinsics)  
             
             num_train_samples = len(train_dataset)
         else:
@@ -296,7 +298,7 @@ class Trainer:
 
         print("Training")
 
-        for batch_idx, inputs in enumerate(self.train_loader):
+        for batch_idx, inputs in enumerate(tqdm(self.train_loader)):
 
             before_op_time = time.time()
 
@@ -546,6 +548,11 @@ class Trainer:
 
             _, depth = disp_to_depth(disp, self.opt.min_depth, self.opt.max_depth)
 
+            """ # APPLY MASK TO DEPTH
+            if self.train_loader.dataset.mask_path:                
+                
+                depth[:,:,self.train_loader.dataset.resized_mask==0]=0 """
+
             outputs[("depth", 0, scale)] = depth
 
             source_scale = 0
@@ -584,10 +591,10 @@ class Trainer:
                 outputs[("position_depth", scale, frame_id)] = self.position_depth[source_scale](
                         cam_points, inputs[("K", source_scale)], T)
 
-    def compute_reprojection_loss(self, pred, target, mask_path='/Users/aure/Documents/CARES/code/mono_reconstruction/data/zoom_masks/3.png'):
-        if mask_path:
+    def compute_reprojection_loss(self, pred, target):
+        """ if self.opt.mask_path:
             # load png mask image
-            image = cv2.imread(mask_path)
+            image = cv2.imread(self.opt.mask_path)
             image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             # convert img to mask
             mask = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)[1]
@@ -598,11 +605,13 @@ class Trainer:
             mask = cv2.resize(mask, (self.opt.height, self.opt.width))
             # make size of mask repeated to match batch and RGB (size 12, 3, x,y)
 
-            mask = np.tile(mask, (self.opt.batch_size, 3, 1, 1)).shape
+            mask = np.tile(mask, (self.opt.batch_size, 3, 1, 1))
+            #print(mask.shape)
 
-        # all areas outside mask should be set to 0 in predicted image
-        pred[mask==0]=0
-        target[mask==0]=0
+            # all areas outside mask should be set to 0 in predicted image
+            pred[mask==0]=0
+            target[mask==0]=0 """
+        
         abs_diff = torch.abs(target - pred)
         l1_loss = abs_diff.mean(1, True)
 